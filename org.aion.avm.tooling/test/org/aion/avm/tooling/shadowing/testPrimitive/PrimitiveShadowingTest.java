e3b4a6438162a22b187d8898dae418b6efd55896
aion-kelvin <kelvin@aion.network>
2019-04-26 17:26:41 -0400

    AKI-121: Add tests for Character

--- org.aion.avm.tooling/test/org/aion/avm/tooling/shadowing/testPrimitive/PrimitiveShadowingTest.java
+++ org.aion.avm.tooling/test/org/aion/avm/tooling/shadowing/testPrimitive/PrimitiveShadowingTest.java
@@ -1,14 +1,20 @@
 package org.aion.avm.tooling.shadowing.testPrimitive;
 
-import org.aion.avm.core.util.ABIUtil;
+import static org.junit.Assert.fail;
+
 import avm.Address;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.math.BigInteger;
+import org.aion.avm.core.util.ABIUtil;
 import org.aion.avm.tooling.AvmRule;
+import org.aion.avm.tooling.AvmRule.ResultWrapper;
+import org.aion.kernel.AvmTransactionResult;
+import org.aion.vm.api.interfaces.TransactionResult;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
-import java.math.BigInteger;
 
 public class PrimitiveShadowingTest {
     @Rule
@@ -80,6 +86,58 @@ public class PrimitiveShadowingTest {
         Object result = avmRule.call(from, dappAddr, BigInteger.ZERO, txData, energyLimit, energyPrice).getDecodedReturnData();
 
         Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void testCharacter() {
+        byte[] txData = ABIUtil.encodeMethodArguments("testCharacter");
+        ResultWrapper result = avmRule.call(from, dappAddr, BigInteger.ZERO, txData,
+            energyLimit, energyPrice);
+
+        TransactionResult tr = result.getTransactionResult();
+        if(! (tr instanceof AvmTransactionResult)) {
+            throw new RuntimeException("Expected result to be of type AvmTransactionResult");
+        }
+        AvmTransactionResult atr = (AvmTransactionResult) tr;
+
+        // The test program throws AssertionError for test failures.  Look for AssertionError
+        // and if found, fail this test and display the stack trace for reporting/informational
+        // purposes.  If any other kind of uncaught exception present, but not AssertionError,
+        // throw an error.
+        if(atr.getUncaughtException() != null) {
+            Throwable outerEx = atr.getUncaughtException();
+            Object unwrapped =
+                ((org.aion.avm.exceptionwrapper.org.aion.avm.shadow.java.lang.Throwable) outerEx).unwrap();
+
+            if(unwrapped instanceof org.aion.avm.shadow.java.lang.AssertionError) {
+                fail("Test failed.  Reason: "
+                    + ((org.aion.avm.shadow.java.lang.AssertionError) unwrapped).avm_getMessage()
+                    + System.lineSeparator()
+                    + throwableAsPrettyString(outerEx));
+            } else if (unwrapped instanceof org.aion.avm.shadow.java.lang.Throwable){
+                throw new RuntimeException("Test error.  Reason: "
+                    + ((org.aion.avm.shadow.java.lang.Throwable) unwrapped).avm_getMessage()
+                    + System.lineSeparator()
+                    + throwableAsPrettyString(outerEx));
+            } else {
+                throw new RuntimeException("Test error.  Reason: "
+                    + ((org.aion.avm.shadow.java.lang.Throwable) unwrapped).avm_getMessage()
+                    + System.lineSeparator()
+                    + throwableAsPrettyString(outerEx));
+            }
+        }
+        Assert.assertEquals(true, result.getDecodedReturnData());
+
+    }
+
+    private static String throwableAsPrettyString(Throwable t) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        t.printStackTrace(pw);
+        String stStr = sw.toString();
+        return "----- Avm uncaught exception stack trace start ------ "
+            + System.lineSeparator() + stStr + System.lineSeparator()
+            + "----- End of Avm uncaught exception ------";
     }
 
     @Test
